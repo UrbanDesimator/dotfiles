3644d953e1e553ceb7ad9179797665bd58a935df
UrbanMusic <proaudiomanuk@gmail.com>
2024-12-23 05:35:34 +0000

    more updates

--- xfce-startupscripts/disable-dither.sh
+++ xfce-startupscripts/disable-dither.sh
@@ -7,8 +7,8 @@
 #
 xrandr --output DP-1 --set "dithering mode" "off"
 #xrandr --output HDMI-1 --set "dithering mode" "off"
-xrandr --output HDMI-1 --set "dithering mode" "auto"
-xrandr --output HDMI-1 --set "dithering depth" "8 bpc"
+xrandr --output HDMI-1 --set "dithering mode" "off"
+#xrandr --output HDMI-1 --set "dithering depth" "8 bpc"
 #xrandr --output HDMI-1 --set "vibrant hue" "100"
 #xrandr --output HDMI-1 --set "color vibrance" "160"
 #
